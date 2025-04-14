dfc003d46fd680f69bac76ae6614684880baca7a
Paramveer Singh <paramveer7267@gmail.com>
2025-04-14 21:13:27 +0530

    first commit

---
+++ constants/theme.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  primary:"#3B82F6",
+  secondary:"#2DD4BF",
+  background:"#000000",
+  surface:"#1A1A1A",
+  surfaceLight:"#4ADE80",
+  white:"#FFFFFF",
+  grey:"#9CA3AF",
+} as const;
